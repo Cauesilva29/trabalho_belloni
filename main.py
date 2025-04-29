cdca5531df26ee9251567dace48259d619cf36ae
Cauesilva29 <Caueinaldo1@gmail.com>
2025-04-28 22:24:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+#Nome:Nome do Aluno
+#RA:28320406
+
+#EXTRACT
+url = "https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv"
+df = pd.read_csv(url)
+
+#TRASNFORM
+df = df[df['Country']=='Brazil'].dropna()
+df['Total Cases'] = df['Confirmed'].cumsum()
+
+print(df.head())
+
+
+#LOAD
+engine = create_engine('sqlite:///covid_brasil.sqlite')
+df.to_sql('brasil_covid',engine, if_exists='replace', index=False)
